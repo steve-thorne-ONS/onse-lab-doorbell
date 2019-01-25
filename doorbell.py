a9faf33784e16c59d8f3b41ac404b898d2626c33
Tom Oram <tom.oram@armakuni.com>
2019-01-25 17:09:40 +0000

    Complete 3 and 5 version

--- doorbell.py
+++ doorbell.py
@@ -1,2 +1,14 @@
 def ring(number_of_knocks):
-    pass
+    is_divisible_by_3 = number_of_knocks % 3 == 0
+    is_divisible_by_5 = number_of_knocks % 5 == 0
+
+    if is_divisible_by_3 and is_divisible_by_5:
+        return 'DingDong'
+
+    if is_divisible_by_3:
+        return 'Ding'
+
+    if is_divisible_by_5:
+        return 'Dong'
+
+    return str(number_of_knocks)
--- test_doorbell.py
+++ test_doorbell.py
@@ -3,3 +3,39 @@ import doorbell
 
 def test_ring_returns_a_string():
     assert type(doorbell.ring(1)) is str
+
+
+def test_ring_returns_1_when_called_with_1():
+    assert doorbell.ring(1) == '1'
+
+
+def test_ring_returns_2_when_called_with_2():
+    assert doorbell.ring(2) == '2'
+
+
+def test_ring_returns_7_when_called_with_7():
+    assert doorbell.ring(7) == '7'
+
+
+def test_ring_returns_ding_when_called_with_3():
+    assert doorbell.ring(3) == 'Ding'
+
+
+def test_ring_returns_ding_when_called_with_6():
+    assert doorbell.ring(6) == 'Ding'
+
+
+def test_ring_returns_dong_when_called_with_5():
+    assert doorbell.ring(5) == 'Dong'
+
+
+def test_ring_returns_dong_when_called_with_10():
+    assert doorbell.ring(10) == 'Dong'
+
+
+def test_ring_returns_dingdong_when_called_with_15():
+    assert doorbell.ring(15) == 'DingDong'
+
+
+def test_ring_returns_dingdong_when_called_with_30():
+    assert doorbell.ring(30) == 'DingDong'
