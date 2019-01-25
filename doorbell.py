7441a31edd8bd8da4f221f449246c29b50c8ef5e
Tom Oram <tom.oram@armakuni.com>
2019-01-25 17:18:51 +0000

    Compelete DingDangDong version

--- doorbell.py
+++ doorbell.py
@@ -1,14 +1,20 @@
 def ring(number_of_knocks):
     is_divisible_by_3 = number_of_knocks % 3 == 0
     is_divisible_by_5 = number_of_knocks % 5 == 0
+    is_divisible_by_7 = number_of_knocks % 7 == 0
 
-    if is_divisible_by_3 and is_divisible_by_5:
-        return 'DingDong'
+    result = ''
 
     if is_divisible_by_3:
-        return 'Ding'
+        result += 'Ding'
 
     if is_divisible_by_5:
-        return 'Dong'
+        result += 'Dang'
 
-    return str(number_of_knocks)
+    if is_divisible_by_7:
+        result += 'Dong'
+
+    if result == '':
+        result = str(number_of_knocks)
+
+    return result
--- test_doorbell.py
+++ test_doorbell.py
@@ -13,10 +13,6 @@ def test_ring_returns_2_when_called_with_2():
     assert doorbell.ring(2) == '2'
 
 
-def test_ring_returns_7_when_called_with_7():
-    assert doorbell.ring(7) == '7'
-
-
 def test_ring_returns_ding_when_called_with_3():
     assert doorbell.ring(3) == 'Ding'
 
@@ -25,17 +21,37 @@ def test_ring_returns_ding_when_called_with_6():
     assert doorbell.ring(6) == 'Ding'
 
 
-def test_ring_returns_dong_when_called_with_5():
-    assert doorbell.ring(5) == 'Dong'
+def test_ring_returns_dang_when_called_with_5():
+    assert doorbell.ring(5) == 'Dang'
 
 
-def test_ring_returns_dong_when_called_with_10():
-    assert doorbell.ring(10) == 'Dong'
+def test_ring_returns_dang_when_called_with_10():
+    assert doorbell.ring(10) == 'Dang'
 
 
-def test_ring_returns_dingdong_when_called_with_15():
-    assert doorbell.ring(15) == 'DingDong'
+def test_ring_returns_dong_when_called_with_7():
+    assert doorbell.ring(7) == 'Dong'
 
 
-def test_ring_returns_dingdong_when_called_with_30():
-    assert doorbell.ring(30) == 'DingDong'
+def test_ring_returns_dong_when_called_with_14():
+    assert doorbell.ring(14) == 'Dong'
+
+
+def test_ring_returns_dingdang_when_called_with_15():
+    assert doorbell.ring(15) == 'DingDang'
+
+
+def test_ring_returns_dingdang_when_called_with_30():
+    assert doorbell.ring(30) == 'DingDang'
+
+
+def test_ring_returns_dingdong_when_called_with_21():
+    assert doorbell.ring(21) == 'DingDong'
+
+
+def test_ring_returns_dangdong_when_called_with_35():
+    assert doorbell.ring(35) == 'DangDong'
+
+
+def test_ring_returns_dingdangdong_when_called_with_105():
+    assert doorbell.ring(105) == 'DingDangDong'
