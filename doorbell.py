2f6c05c7f6652c8971fa049c84a0f8c50afe9bd6
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2019-02-11 14:16:28 +0000

    Phils first coding attempt

--- doorbell.py
+++ doorbell.py
@@ -1,2 +1,13 @@
 def ring(number_of_knocks):
-    pass
+    doorbell_response = ""
+
+    if number_of_knocks % 3 == 0:
+        doorbell_response = doorbell_response + "Ding"
+    if number_of_knocks % 5 == 0:
+        doorbell_response = doorbell_response + "Dang"
+    if number_of_knocks % 7 == 0:
+        doorbell_response = doorbell_response + "Dong"
+    if doorbell_response == "":
+        return number_of_knocks
+    return doorbell_response
+
--- test_doorbell.py
+++ test_doorbell.py
@@ -1,5 +1,41 @@
 import doorbell
 
 
-def test_ring_returns_a_string():
-    assert type(doorbell.ring(1)) is str
+def test_ring_returns_an_int():
+    print(doorbell.ring(106))
+    assert type(doorbell.ring(106)) is int
+
+
+def test_ring_returns_ding():
+    print(doorbell.ring(6))
+    assert doorbell.ring(6) == "Ding"
+
+
+def test_ring_returns_dong():
+    print(doorbell.ring(5))
+    assert doorbell.ring(5) == "Dang"
+
+
+def test_ring_returns_dingdang():
+    print(doorbell.ring(15))
+    assert doorbell.ring(15) == "DingDang"
+
+
+def test_ring_returns_dang():
+    print(doorbell.ring(7))
+    assert doorbell.ring(7) == "Dong"
+
+
+def test_ring_returns_dingdangdong():
+    print(doorbell.ring(105))
+    assert doorbell.ring(105) == "DingDangDong"
+
+
+def test_ring_returns_dangdong():
+    print(doorbell.ring(35))
+    assert doorbell.ring(35) == "DangDong"
+
+
+def test_ring_returns_dingdong():
+    print(doorbell.ring(21))
+    assert doorbell.ring(21) == "DingDong"
